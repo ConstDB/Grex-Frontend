c53385f3ce123a24d1c1f3ec07f5f58756ac7082
Jonel Villaver <jonelvillaver735@gmail.com>
2025-08-27 07:52:28 +0800

    feat(subtask): simulate subtask deletion

--- src/features/tasks/components/Subtask.tsx
+++ src/features/tasks/components/Subtask.tsx
@@ -2,6 +2,7 @@ import { Checkbox } from "@/components/ui/checkbox";
 import { cn } from "@/lib/utils";
 import { useSubtaskStore } from "@/stores/useSubtasksStore";
 import type { Subtask } from "@/types/task";
+import { Trash } from "lucide-react";
 
 type Props = {
   subtask: Subtask;
@@ -10,9 +11,11 @@ type Props = {
 export default function SubtaskItem({ subtask }: Props) {
   // THIS WILL BE A FUNCTION THAT TRIGGERS A PATCH REQUEST FOR TOGGLING is_done FIELD IN THE SERVER
   const toggleSubtask = useSubtaskStore((state) => state.toggleSubtask);
+  // THIS WILL BE A REQUEST THAT DELETES THE SUBTASK
+  const deleteSubtask = useSubtaskStore((state) => state.deleteSubtask);
 
   return (
-    <li key={subtask.subtask_id} className="flex items-center gap-2 px-4">
+    <li key={subtask.subtask_id} className="group flex items-center gap-2 px-4">
       <Checkbox
         checked={subtask.is_done}
         onCheckedChange={() => toggleSubtask(subtask.subtask_id)}
@@ -25,6 +28,9 @@ export default function SubtaskItem({ subtask }: Props) {
       >
         {subtask.description}
       </p>
+      <button onClick={() => deleteSubtask(subtask.subtask_id)}>
+        <Trash className="size-4 text-error/50 hidden group-hover:block" />
+      </button>
     </li>
   );
 }
