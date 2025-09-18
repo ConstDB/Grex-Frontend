1b7488079f2207779c21f604545ffd1a7dc0ca76
Jonel Villaver <jonelvillaver735@gmail.com>
2025-09-18 23:51:40 +0800

    types(task): finalize comment and attachment types

--- src/types/comment.ts
+++ src/types/comment.ts
@@ -1,12 +1,30 @@
 export interface NewComment {
-  content: string;
+  content?: string;
   sender_id: number;
+  attachment?: NewCommentAttachment;
 }
 
-export interface Comment extends NewComment {
+export interface NewCommentAttachment {
+  name: string;
+  file_size: number;
+  file_type: string;
+  file_url: string;
+  uploaded_by: number;
+  uploaded_at: Date;
+}
+
+export interface TaskAttachment extends NewCommentAttachment {
+  comment_id: number;
+  attachment_id: number;
+}
+
+export interface Comment extends Omit<NewComment, "attachment"> {
   comment_id: number;
   task_id: number;
   created_at: Date;
   profile_picture: string;
-  sender_name: string; 
+  sender_name: string;
+  attachment?: TaskAttachment;
 }
+
+
